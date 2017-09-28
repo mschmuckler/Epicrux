8af0a470fae8f796fd645039e5a94cefdc85525a
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-09-28 15:33:24 -0400

    Add clear error action

--- frontend/actions/error_actions.js
+++ frontend/actions/error_actions.js
@@ -1,9 +1,17 @@
 export const RECEIVE_ERROR = "RECEIVE_ERROR";
+export const CLEAR_ERROR = "CLEAR_ERROR";
 
 export const receiveError = (error, errorType) => {
   return {
     type: RECEIVE_ERROR,
     errorType,
     error,
+  };
+};
+
+export const clearError = (errorType) => {
+  return {
+    type: CLEAR_ERROR,
+    errorType,
   };
 };
--- frontend/reducers/error_reducer.js
+++ frontend/reducers/error_reducer.js
@@ -1,9 +1,14 @@
-import { RECEIVE_ERROR } from '../actions/error_actions.js';
+import {
+  RECEIVE_ERROR,
+  CLEAR_ERROR,
+} from '../actions/error_actions.js';
 
 const ErrorReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_ERROR:
       return { [action.errorType]: action.error };
+    case CLEAR_ERROR:
+      return { [action.errorType]: null };
     default:
       return state;
   }
