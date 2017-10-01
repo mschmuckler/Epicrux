75c453491f9b6e0cd45517f8e2f7c3b74303690f
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-10-01 11:20:45 -0400

    Add datetime, duration and category inputs on seizure form

--- frontend/components/seizures/seizure_form.jsx
+++ frontend/components/seizures/seizure_form.jsx
@@ -1,14 +1,63 @@
 import React from 'react';
 
+
 class SeizureForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      datetime: "",
+      durationMinutes: "",
+      durationSeconds: "",
+      category: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(type) {
+    return (e) => {
+      this.setState({ [type]: e.target.value });
+    };
+  }
+
+  printState() {
+    console.log(this.state);
   }
 
   render() {
     return (
       <div>
-        Form input
+        <h1>Seizure Input Form</h1>
+        <form>
+          <input
+            value={ this.state.datetime }
+            onChange={ this.handleChange("datetime") }
+            type="datetime-local"
+          />
+        Seconds:{ this.state.durationSeconds }
+          <input
+            value={ this.state.durationSeconds }
+            onChange={ this.handleChange("durationSeconds") }
+            type="range"
+            max="59"
+          />
+        Minutes:{ this.state.durationMinutes }
+          <input
+            value={ this.state.durationMinutes }
+            onChange={ this.handleChange("durationMinutes") }
+            type="range"
+            max="15"
+          />
+        <select
+          value={ this.state.category }
+          onChange={ this.handleChange("category") }
+        >
+          <option disabled value="">--Select a Category--</option>
+          <option value="aura" >Aura</option>
+          <option value="fdsf" >fsdf</option>
+        </select>
+        </form>
+        <button onClick={ this.printState.bind(this) } >getState</button>
       </div>
     );
   }
--- frontend/components/seizures/seizure_form_container.jsx
+++ frontend/components/seizures/seizure_form_container.jsx
@@ -2,6 +2,12 @@ import { connect } from 'react-redux';
 import { addSeizure } from '../../actions/seizure_actions';
 import SeizureForm from './seizure_form';
 
+const mapStateToProps = (state) => {
+  return {
+    errors: state.errors.seizure,
+  };
+};
+
 const mapDispatchToProps = (dispatch) => {
   return {
     addSeizure: (seizureInput) => dispatch(addSeizure(seizureInput)),
